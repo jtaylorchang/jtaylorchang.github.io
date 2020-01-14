56a9fb9a5a85f9699e01d39156d611d955468ab2
Jeff Taylor-Chang <jeff@jefftc.com>
2020-01-14 14:31:47 -0500

    Updates

---
+++ precache-manifest.720ad688a4b0a27a0370a932831012cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b319e031766e0a4f805a5cff71e1752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069f85c19a243827c6a2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "221f5a7de7be47109039",
+    "url": "/static/js/2.9303ad9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9303ad9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "069f85c19a243827c6a2",
+    "url": "/static/js/main.55e40ad8.chunk.js"
+  },
+  {
+    "revision": "25ad0c105d8df6373ccc",
+    "url": "/static/js/runtime-main.e40eaf9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
